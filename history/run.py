8b0046ca3ab383293b22aabda21b5563ee08a5b0
Nikita Chepanov <nikita@chepanov.com>
2014-06-10 04:01:11 -0400

    dirty example of using python to get access bigqeury

--- history/run.py
+++ history/run.py
@@ -1,10 +1,74 @@
 #!/usr/bin/python
-from github import Github
-import sys
+import httplib2
 
-g = Github(login_or_token='74ff4320f6b54cc4bf74dc4f006661a782e31418')
-u = g.get_user(sys.argv[1])
-for repo in u.get_repos():
-	print repo.name
+from apiclient.discovery import build
+from apiclient.errors import HttpError
 
-print '-'*23 + "\nRate limit: %s" % (g.rate_limiting,)
+from oauth2client.client import SignedJwtAssertionCredentials
+from oauth2client.client import AccessTokenRefreshError
+
+PROJECT_ID = 'astral-sunbeam-600'
+SERVICE_ACCOUNT_EMAIL = '766106295283-mvbdl76eduioip1aa96u9b0h6d9kut4n@developer.gserviceaccount.com'
+
+f = file('cefb7ce33f1376da26ff6f33a7c8397fbf8fee68-privatekey.p12', 'rb')
+key = f.read()
+f.close()
+
+credentials = SignedJwtAssertionCredentials(
+    SERVICE_ACCOUNT_EMAIL,
+    key,
+    scope='https://www.googleapis.com/auth/bigquery')
+
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+service = build('bigquery', 'v2', http=http)
+
+def printTableData(data, startIndex):
+  for row in data['rows']:
+    rowVal = []
+    for cell in row['f']:
+        rowVal.append(cell['v'])
+    print 'Row %d: %s' % (startIndex, rowVal)
+    startIndex +=1
+
+# Run a synchronous query, save the results to a table, overwriting the
+# existing data, and print the first page of results.
+# Default timeout is to wait until query finishes.
+def runSyncQuery (service, projectId, datasetId='samples', timeout=0):
+	print 'timeout:%d' % timeout
+	jobCollection = service.jobs()
+	queryData = {'query':'SELECT word,count(word) AS count FROM publicdata:samples.shakespeare GROUP BY word;',
+		         'timeoutMs':timeout}
+
+	queryReply = jobCollection.query(projectId=projectId,
+		                             body=queryData).execute()
+
+	jobReference=queryReply['jobReference']
+
+	# Timeout exceeded: keep polling until the job is complete.
+	while(not queryReply['jobComplete']):
+	  print 'Job not yet complete...'
+	  queryReply = jobCollection.getQueryResults(
+		                  projectId=jobReference['projectId'],
+		                  jobId=jobReference['jobId'],
+		                  timeoutMs=timeout).execute()
+
+	# If the result has rows, print the rows in the reply.
+	if('rows' in queryReply):
+	  print 'has a rows attribute'
+	  printTableData(queryReply, 0)
+	  currentRow = len(queryReply['rows'])
+
+	  # Loop through each page of data
+	  while('rows' in queryReply and currentRow < queryReply['totalRows']):
+		queryReply = jobCollection.getQueryResults(
+		                  projectId=jobReference['projectId'],
+		                  jobId=jobReference['jobId'],
+		                  startIndex=currentRow).execute()
+		if('rows' in queryReply):
+		  printTableData(queryReply, currentRow)
+		  currentRow += len(queryReply['rows'])
+
+
+runSyncQuery(service, PROJECT_ID)
