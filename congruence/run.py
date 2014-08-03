e2d8b2202702c717fcf9b69be10cbabea73f3e05
Nikita Chepanov <nikita@chepanov.com>
2014-08-03 18:10:18 -0400

    Can compare

--- congruence/run.py
+++ congruence/run.py
@@ -22,20 +22,60 @@ def diff(A,B):
 	n = len(A) # they expected to be of the same length
 	return [get_cosine(A[i], B[i]) for i in range(n)]
 
+
+def sparsity(m):
+	l = len(m)
+	total = l*l
+	zero = 0
+	for i in range(l):
+		for j in range(l):
+			if m[i][j] == 0:
+				zero+=1
+	return total - zero, total
+
+def  transform(m, index, allowed):
+	l = len(allowed)
+	reverse_index = {user:idx for idx, user in enumerate(index)}
+	out = [[0]*l for _ in range(l)]
+	for i in range(l):
+		user_i  = allowed[i]
+		ii = reverse_index[user_i]
+		for j in range(l):
+			user_j  = allowed[j]
+			jj = reverse_index[user_j]
+			out[i][j] = m[ii][jj]
+	return out
+
 def main():
-	history = sys.argv[1]
-	actual = sys.argv[2]
-	with open(history, 'r') as hfile:
-		hjson = json.load(hfile)
-	with open(actual, 'r') as afile:
-		ajson = json.load(afile)
-	people = ajson["people"]
-	H = hjson["matrix"]	  	# historical
-	T = ajson["requirements"] 	# technical
-	E = ajson["comments"]		# explicit
-	H2T = diff(H, T)
-	T2E = diff(T, E)
-	for i in len(H):
-		print people[i], "\t", H2T[i], T2E[i]
+	repos = []
+	HISTORY= "../history/output_data/{}.json"
+	ACTUAL = "../technical/output_data/{}"
+	with open(sys.argv[1]) as f:
+		repos = f.readlines()
+	for repo_line in repos:
+		try:
+			repo = repo_line.strip().replace("/","-")
+			with open(HISTORY.format(repo), 'r') as hfile:
+				hjson = json.load(hfile)
+			with open(ACTUAL.format(repo), 'r') as afile:
+				ajson = json.load(afile)
+			hpeople = hjson["users"]
+			apeople = ajson["people"]
+			people = list(set(hpeople) & set(apeople))
+			H = hjson["matrix"]	  	# historical
+			T = ajson["requirements"] 	# technical
+			E = ajson["comments_matrix"]		# explicit
+			print sparsity(H), sparsity(T), sparsity(E)
+			H = transform(H, hpeople, people)
+			T = transform(T, apeople, people)
+			E = transform(E, apeople, people)
+			print sparsity(H), sparsity(T), sparsity(E)
+			H2T = diff(H, T)
+			T2E = diff(T, E)
+			H2T_norm = math.sqrt(sum(i**2 for i in H2T))
+			T2E_norm = math.sqrt(sum(i**2 for i in T2E))
+			print "H2T", H2T_norm, "T2E", T2E_norm
+		except Exception as e:
+			print "Error with ", repo, ".", e
 
 main()
