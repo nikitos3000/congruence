04efadb7b5347d19ceabbe03a38b3b9ecf66cd9f
Nikita <nikitos3000@gmail.com>
2014-08-04 01:50:09 -0400

    .

--- technical/driver.sh
+++ technical/driver.sh
@@ -7,8 +7,9 @@ do
 	echo "Begin:\t$now\t$line"
 	python script.py $line > script.log 2>&1
 	rc=$?
+	cat script.log >> all.log
 	now=$(date +"%a %T")
-	if [[ $rc != 0 ]] ; then
+	if [ $rc -ne  0 ] ; then
 		echo "ERROR:\t$now"
 	else 
 		echo "Done:\t$now"
